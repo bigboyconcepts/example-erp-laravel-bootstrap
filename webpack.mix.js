b8c2107022d9b541f7d5cd403b29c65f5a06fa4f
Afonso Gloeden <afgloeden@gmail.com>
2018-11-29 16:48:21 -0200

    .extract() and .version() added to webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,14 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+   .sass('resources/sass/app.scss', 'public/css')
+   .extract([
+      'jquery',
+      'popper.js',
+      'bootstrap',
+      '@fortawesome/fontawesome-free/js/solid.js',
+      '@fortawesome/fontawesome-free/js/regular.js',
+      '@fortawesome/fontawesome-free/js/brands.js',
+      '@fortawesome/fontawesome-free/js/fontawesome.js'
+   ])
+   .version();
