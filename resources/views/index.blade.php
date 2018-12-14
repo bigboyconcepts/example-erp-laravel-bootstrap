277609465604118b2aae26ab9de53d87a64fb435
Afonso Gloeden <afgloeden@gmail.com>
2018-12-14 11:03:48 -0200

    View index updated

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-  <h1 class="text-center m-5"><i class="fas fa-project-diagram"></i> ERP</h1>
+  <h1 class="text-center m-5">{{ config('app.name') }}</h1>
 @endsection
