2b46e0ad877889aabdc7f58d5f71eefeaca90c93
Afonso Gloeden <afgloeden@gmail.com>
2018-12-14 22:01:48 -0200

    Home and auth routes added in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
-Route::get('/', function () {
-    return view('index');
+Auth::routes(['verify' => true]);
+Route::redirect('/', '/home');
+Route::get('/profile', 'Auth\ProfileController@index')->name('profile.index');
+Route::middleware('verified')->group(function() {
+    Route::get('/home', 'HomeController@index')->name('home');
 });
