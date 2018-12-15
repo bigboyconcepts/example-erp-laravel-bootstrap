4aebde464dde29c666d78391ba6003cbd6532b63
Afonso Gloeden <afgloeden@gmail.com>
2018-12-14 22:01:13 -0200

    JS removed from webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,7 @@
 const mix = require('laravel-mix');
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
-   .extract([
-      'jquery',
-      'popper.js',
-      'bootstrap',
-      '@fortawesome/fontawesome-free/js/solid.js',
-      '@fortawesome/fontawesome-free/js/regular.js',
-      '@fortawesome/fontawesome-free/js/brands.js',
-      '@fortawesome/fontawesome-free/js/fontawesome.js'
-   ])
-   if (mix.inProduction()) {
-      mix.version();
-   }
+mix.sass('resources/sass/app.scss', 'public/css');
+
+if (mix.inProduction()) {
+    mix.version();
+}
